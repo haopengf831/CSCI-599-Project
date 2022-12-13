6cbff1da0f90e8c1333c011c7697629b55aa4378
Haopeng <hf0242a@student.american.edu>
2022-12-13 07:52:11 -0800

    111

--- PythonAPI/venv/main.py
+++ PythonAPI/venv/main.py
@@ -3,9 +3,14 @@ import os
 import keyboard
 import mousePosition
 import time
+import pyautogui as pag
+
+screenWidth, screenHeight = pag.size()
+
 
 # connect to the AirSim simulator
 client = airsim.MultirotorClient()
+
 client.confirmConnection()
 client.enableApiControl(True)
 client.armDisarm(True)
@@ -14,6 +19,11 @@ client.armDisarm(True)
 client.takeoffAsync().join()
 client.moveToPositionAsync(0, 0, 0, 5).join()
 client.hoverAsync().join()
+# time.sleep(10)
+# client.moveToPositionAsync(5, 14, 0, 5).join()
+
+# print(client.getMultirotorState())
+
 
 # time.sleep(3)
 # client.moveToPositionAsync(0, 0, -5.1, 3).join()
@@ -36,11 +46,67 @@ for response in responses:
         print("Type %d, size %d" % (response.image_type, len(response.image_data_uint8)))
         airsim.write_file(os.path.normpath('/temp/py1.png'), response.image_data_uint8)
 
+notMove = False
+count = 0
+zaxis = 0
+m = 0
+n = 0
 while True:
+    # if(mousePosition.abc() == True):
+    #     print("Moving back")
+
+
     keyboard.wait('m')
-    list = mousePosition.func()
-    print("Current position" ,list)
-    client.moveToPositionAsync(0, list[0], list[1], 3).join()
+    a, b = pag.position()
+    if((a>500 and a<900) and (b>14 and b<84)):
+        zaxis = zaxis + 5
+        client.moveToPositionAsync(zaxis, m, n, 3).join()
+    elif ((a > 980 and a < 1380) and (b > 14 and b < 84)):
+        zaxis = zaxis - 5
+        client.moveToPositionAsync(zaxis, m, n, 3).join()
+    else:
+        list = mousePosition.func()
+        print("Current position" ,list)
+        if(list[0]>=0 and list[1]>=0):
+            m = list[0]+zaxis
+            n = list[1]+ 0.56*zaxis
+            client.moveToPositionAsync(0.01 + zaxis, m, n, 3).join()
+        if (list[0] < 0 and list[1] >= 0):
+            m = list[0] - zaxis
+            n = list[1] + 0.56 * zaxis
+            client.moveToPositionAsync(0.01 + zaxis, m, n, 3).join()
+        if (list[0] < 0 and list[1] < 0):
+            m = list[0] - zaxis
+            n = list[1] - 0.56 * zaxis
+            client.moveToPositionAsync(0.01 + zaxis, m, n, 3).join()
+        if (list[0] >= 0 and list[1] < 0):
+            m = list[0] + zaxis
+            n = list[1] - 0.56 * zaxis
+            client.moveToPositionAsync(0.01 + zaxis, m, n, 3).join()
+        client.hoverAsync().join()
+    # time.sleep(1)
+    # c, d = pag.position()
+    # if (abs(c - a) < 150 and abs(d - b) < 150):
+    #     count = count + 1
+    # time.sleep(1)
+    # e, f = pag.position()
+    # if (abs(e - a) < 150 and abs(f - b) < 150):
+    #     count = count + 1
+    # if(count == 2):
+    #     notMove = True
+    #     zaxis = zaxis + 5
+    #     print("not moving")
+    #     client.moveToPositionAsync(zaxis, m, n, 3).join()
+    #     count = 0
+    # time.sleep(1)
+    # notMove = False
+    # count = 0
+
+
+    # time.sleep(10)
+    # print(client.getMultirotorState())
+
+
 
 
 
--- PythonAPI/venv/mousePosition.py
+++ PythonAPI/venv/mousePosition.py
@@ -2,6 +2,7 @@ import os
 import time
 import pyautogui as pag
 
+screenWidth, screenHeight = pag.size()
 def  func():
         screenWidth, screenHeight = pag.size()
         x, y = pag.position()
@@ -14,6 +15,28 @@ def  func():
 
         os.system('cls')
         return list
+
+# notMove = False
+# while True:
+#         while True:
+#                 a, b = pag.position()
+#                 time.sleep(1)
+#                 c, d = pag.position()
+#                 if(abs(c-a) > 150 or abs(d-b) > 150):
+#                         break
+#                 time.sleep(1)
+#                 e, f = pag.position()
+#                 if(abs(e-a) > 150 or abs(f-b) > 150):
+#                       break
+#                 notMove = True
+#
+#                 time.sleep(1)
+#                 notMove = False
+#
+#
+# def adc():
+#         return notMove
+
 
 # def getX():
 #     xValue = x
